22c464eb630dea219480374f03bad526c6a7b838
Maks_race <madventurerace@gmail.com>
2022-04-01 22:29:04 +0300

    Changed after feedback

--- src/hillel/Main.java
+++ src/hillel/Main.java
@@ -1,8 +1,14 @@
 package hillel;
 
+import hillel.lesson4.*;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Car accord = new Car("ghj", 57, 2.5);
+        accord.iWantToDrive(100);
+        accord.iWantToDrive(150, 120);
+        accord.fillTank(15);
+        accord.showOdometer();
     }
 }
